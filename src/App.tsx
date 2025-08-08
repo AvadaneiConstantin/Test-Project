aa876bfb1d67a4ca9f26bf69c53d24570d475088
Constantin Avadanei <constantin.avadanei@mendelio.io>
2025-08-08 15:07:28 +0300

    My first Push on Git code

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ function App() {
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Hello from testProject </h1>
       <p>React + TypeScript + Vite project</p>
+      <p>1. Test push </p>
     </div>
   );
 }
