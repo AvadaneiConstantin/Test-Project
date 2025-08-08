142a9a3cb2e60faadc3a045fa2958d106f62534b
Constantin Avadanei <constantin.avadanei@mendelio.io>
2025-08-08 14:40:15 +0300

    Initial commit - Test Project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello from testProject </h1>
+      <p>React + TypeScript + Vite project</p>
+    </div>
+  );
+}
+
+export default App;
